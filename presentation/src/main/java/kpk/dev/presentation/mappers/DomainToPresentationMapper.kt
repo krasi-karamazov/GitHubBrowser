aec0037d7213c0d0beb399ea3915affb6b2f08f1
krasi <krasi.karamazov@gmail.com>
2020-09-14 15:55:14 +0300

    commit date format

--- presentation/src/main/java/kpk/dev/presentation/mappers/DomainToPresentationMapper.kt
+++ presentation/src/main/java/kpk/dev/presentation/mappers/DomainToPresentationMapper.kt
@@ -4,6 +4,8 @@ import kpk.dev.domain.entity.CommitItem
 import kpk.dev.domain.entity.GitHubRepoItem
 import kpk.dev.presentation.model.CommitUiModel
 import kpk.dev.presentation.model.GitHubRepoUiModel
+import java.text.SimpleDateFormat
+import java.util.*
 
 fun GitHubRepoItem.map() = GitHubRepoUiModel(
     id,
@@ -20,8 +22,15 @@ fun GitHubRepoItem.map() = GitHubRepoUiModel(
 
 fun CommitItem.map(repoName: String) = CommitUiModel(
     author,
-    date,
+    formatDate(date),
     message,
     hash,
     repoName
 )
+
+private fun formatDate(dateString: String): String {
+    val serverDateFormat = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssX", Locale.getDefault())
+    val uiDateFormat = SimpleDateFormat("dd/MM/yyyy hh:mm:ss", Locale.getDefault())
+    val serverDate = serverDateFormat.parse(dateString)
+    return uiDateFormat.format(serverDate!!)
+}
